f0335d871bf526d49d6a3f5b2352a93598da37f4
Brandon <brandonjohnflick@gmail.com>
2022-03-03 19:57:35 +0100

    Optimized Record checking  and added default function to add default records

--- index.js
+++ index.js
@@ -19,11 +19,10 @@ require("dotenv").config();
 
 const checkIfARecordExists = async (key, settings) => {
   let exists = false;
-  console.log("Checking");
 
   const existingRecords = await axios({
     method: "GET",
-    url: `https://api.cloudflare.com/client/v4/zones/${settings.zoneId}/dns_records`,
+    url: `https://api.cloudflare.com/client/v4/zones/${settings.zoneId}/dns_records?name=${key}.${settings.domain}`,
     headers: {
       "X-Auth-Email": settings.email,
       "X-Auth-Key": settings.api,
@@ -31,13 +30,34 @@ const checkIfARecordExists = async (key, settings) => {
     },
   });
 
-  existingRecords.data.result.forEach((dns) => {
-    if (dns.name.split(".")[0] === key) {
-      exists = true;
-    }
-  });
+  if (existingRecords.data.result.length >= 1) exists = true;
 
   return exists;
+};
+
+const addDefaultARecords = async (settings) => {
+  ["auth", "traefik"].forEach((record) => {
+    try {
+      await axios({
+        method: "POST",
+        url: `https://api.cloudflare.com/client/v4/zones/${settings.zoneId}/dns_records`,
+        data: {
+          type: "A",
+          name: record,
+          content: await publicIp.v4(),
+          ttl: 1,
+          proxied: false,
+        },
+        headers: {
+          "X-Auth-Email": settings.email,
+          "X-Auth-Key": settings.api,
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  });
 };
 
 (async () => {
@@ -51,12 +71,16 @@ const checkIfARecordExists = async (key, settings) => {
     const cloudFlareApi = await Settings.findOne({ name: "cloudflare_global_api" });
     const cloudFlareZoneId = await Settings.findOne({ name: "cloudflare_zone_id" });
     const cloudFlareEmail = await Settings.findOne({ name: "cloudflare_email" });
+    const domain = await Settings.findOne({ name: "domain" });
 
     const settings = {
       api: cloudFlareApi.value,
       zoneId: cloudFlareZoneId.value,
       email: cloudFlareEmail.value,
+      domain,
     };
+
+    await addDefaultARecords(settings);
 
     events.on("container.create", async (ev) => {
       const attributeKeys = Object.keys(ev.Actor.Attributes);
